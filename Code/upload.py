7c7d832c9f556bb0c9d9fd26950fc701589eaa6a
thomasreus97 <thomas_reus@hotmail.com>
2018-11-03 16:10:58 +0100

    paar grafieken en berekeningen toegevoegd: verdeling huis output min/max cable cost matrix houses output matrix batteries

--- Code/upload.py
+++ Code/upload.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Grid():
     """
@@ -13,7 +14,8 @@ class Grid():
         """
         self.batteries = self.load_batteries(f"Huizen&Batterijen/wijk{district}_batterijen.txt")
         self.houses = self.load_houses(f"Huizen&Batterijen/wijk{district}_huizen.csv")
-        self.distances_battery = self.distances()
+        self.distances_battery = self.distances('battery')
+        self.distances_houses = self.distances()
 
     def load_batteries(self, filename):
         """
@@ -67,7 +69,7 @@ class Grid():
         # return list of houses
         return houses_list
 
-    def distances(self):
+    def distances(self, option=None):
         """
         Calculate manhatten distances between
         houses and every battery, add these
@@ -75,22 +77,29 @@ class Grid():
         Return list with a distance dictionary
         for every battery.
         """
+        if option == 'battery':
+            a = self.batteries
+            b = self.houses
+        else:
+            b = self.batteries
+            a = self.houses
+
         # list for distance dictionaries
         items = []
-        # iterate over batteries
-        for battery in self.batteries:
+        # iterate over a
+        for i in a:
             # dictionary for distances
             distances = {}
             # variables in battery
-            x1, y1, cap1 = battery
+            x1, y1, cap1 = i
             # iterate over houses
-            for house in self.houses:
+            for j in b:
                 # variables in house
-                x2, y2, cap2 = house
+                x2, y2, cap2 = j
                 # calculate manhatten distance
                 d = abs(x1 - x2) + abs(y1 - y2)
                 # add to distances dictionary
-                distances[d] = house
+                distances[d] = j
             # add distances dictionary to items dictionary
             items.append(distances)
         # return list of dictionaries
@@ -100,6 +109,7 @@ class Grid():
         """
         Make a picture of the Grid
         """
+        plt.figure(0)
         # add houses to image
         for house in self.houses:
             plt.plot(house[0], house[1], 'ro')
@@ -107,20 +117,63 @@ class Grid():
         for battery in self.batteries:
             plt.plot(battery[0], battery[1], 'bo')
         # show image
-        plt.show()
+
+    def matrix(self):
+        """
+        Make matrix
+        """
+        matrix_house = np.zeros([51, 51])
+        matrix_battery = np.zeros([51, 51])
+        for i in self.houses:
+            matrix_house[i[1]][i[0]] = i[2]
+        for i in self.batteries:
+            matrix_battery[i[1]][i[0]] = i[2]
+        plt.figure("houses")
+        plt.imshow(matrix_house, origin='lower')
+        plt.colorbar()
+        plt.figure("batteries")
+        plt.imshow(matrix_battery, origin='lower')
+        plt.colorbar()
+
+    def graphs(self):
+        """
+        Create some graphs showing information
+        """
+        # histogram house outpur distribution
+        list = []
+        for i in self.houses:
+            list.append(i[2])
+        print("houses =", len(list))
+        plt.figure("histogram")
+        plt.title("house output distribution")
+        plt.hist(list, bins=5)
 
 # run
 if __name__ == "__main__":
     grid = Grid(1)
 
+    """"calculate min/max cost (if batteries had infinite capacity)"""
+    total_min_distance = 0
+    total_max_distance = 0
+    for i in grid.distances_houses:
+        total_min_distance += min(i)
+        total_max_distance += max(i)
+    print("min cost cables =", total_min_distance*9)
+    print("max cost cables =", total_max_distance*9)
+
+    """Calculate leftover total battery capacity"""
     # print(f"batteries:{grid.batteries}")
     # print(f"houses:{grid.houses}")
-    # grid.plot_grid()
+    total_battery = 0
+    total_houses = 0
+    for i in grid.batteries:
+        total_battery += i[2]
+    for i in grid.houses:
+        total_houses += i[2]
+    print("Leftover battery capacity =", total_battery - total_houses)
 
-    # total_battery = 0
-    # total_houses = 0
-    # for i in grid.batteries:
-    #     total_battery += i[2]
-    # for i in grid.houses:
-    #     total_houses += i[2]
-    # print(total_battery - total_houses)
+    """"Show graphs"""
+    grid.matrix()
+    grid.plot_grid()
+    grid.graphs()
+    plt.show()
