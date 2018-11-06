0acde16c999eb5fcb67aeb21e192ecd75f821280
HarmkeV <44019003+HarmkeV@users.noreply.github.com>
2018-11-06 16:12:58 +0100

    style

--- Code/upload.py
+++ Code/upload.py
@@ -17,6 +17,7 @@ class Grid():
         self.distances_battery = self.distances('battery')
         self.distances_houses = self.distances()
 
+
     def load_batteries(self, filename):
         """
         Open .txt file of batteries
@@ -46,6 +47,7 @@ class Grid():
         # return list of batteries
         return batteries_list
 
+
     def load_houses(self, filename):
         """
         Open .csv file of houses
@@ -69,6 +71,7 @@ class Grid():
         # return list of houses
         return houses_list
 
+
     def distances(self, option=None):
         """
         Calculate manhatten distances between
@@ -86,41 +89,53 @@ class Grid():
 
         # list for distance dictionaries
         items = []
+
         # iterate over a
         for i in a:
+
             # dictionary for distances
             distances = {}
+
             # variables in battery
             x1, y1, cap1 = i
+
             # iterate over houses
             for j in b:
+
                 # variables in house
                 x2, y2, cap2 = j
+
                 # calculate manhatten distance
                 d = abs(x1 - x2) + abs(y1 - y2)
+
                 # add to distances dictionary
                 distances[d] = j
+
             # add distances dictionary to items dictionary
             items.append(distances)
+
         # return list of dictionaries
         return items
+
 
     def plot_grid(self):
         """
         Make a picture of the Grid
         """
         plt.figure(0)
+
         # add houses to image
         for house in self.houses:
             plt.plot(house[0], house[1], 'ro')
+
         # add batteries to image
         for battery in self.batteries:
             plt.plot(battery[0], battery[1], 'bo')
-        # show image
+
 
     def matrix(self):
         """
-        Make matrix
+        Create matrix
         """
         matrix_house = np.zeros([51, 51])
         matrix_battery = np.zeros([51, 51])
@@ -135,11 +150,12 @@ class Grid():
         plt.imshow(matrix_battery, origin='lower')
         plt.colorbar()
 
+
     def graphs(self):
         """
         Create some graphs showing information
         """
-        # histogram house outpur distribution
+        # histogram house output distribution
         list = []
         for i in self.houses:
             list.append(i[2])
@@ -149,6 +165,7 @@ class Grid():
         plt.figure("histogram")
         plt.title("house output distribution")
         plt.hist(list, bins=5)
+
 
 # run
 if __name__ == "__main__":
