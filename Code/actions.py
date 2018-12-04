a750ac57fda2d707d8c846a9faa3249603417dad
HarmkeV <44019003+HarmkeV@users.noreply.github.com>
2018-12-04 11:49:37 +0100

    ding.py ingevoegd bij actions.py als random_plt()

--- Code/actions.py
+++ Code/actions.py
@@ -244,3 +244,46 @@ class Plots():
             plt.pause(0.000000001)
 
         plt.title(f"total cost = {total_cost}")
+
+    def random_plt(self):
+        for i in range(1,4):
+            cost_list = []
+            with open(f"text_info_random{i}_10k.txt", "r") as f:
+                # text = f.read()
+                # cost_list.append(text)
+                text = f.read().split('\n')
+                for number in text:
+
+                    # number = number.replace(' ', '')
+                    # number = number.replace('\n', '')
+                    # number = number.replace('[', '')
+                    # number = number.replace(']', '')
+                    if number is not "":
+                        cost_list.append(int(number))
+                    # print(number, type(number))
+            # bins = np.linspace(min(cost_list), max(cost_list))
+            plt.hist(cost_list, bins=30, alpha=0.5, label=f"Random Wijk {i}")
+
+
+            cost_list = []
+            with open(f"text_info_prior_hill{i}_1k.txt", "r") as f:
+                # text = f.read()
+                # cost_list.append(text)
+                text = f.read().split('\n')
+                for number in text:
+
+                    # number = number.replace(' ', '')
+                    # number = number.replace('\n', '')
+                    # number = number.replace('[', '')
+                    # number = number.replace(']', '')
+                    if number is not "":
+                        cost_list.append(int(number))
+                    # print(number, type(number))
+            # bins = np.linspace(min(cost_list), max(cost_list))
+            plt.hist(cost_list, bins=5, alpha=1, label=f"Hill Wijk {i}")
+
+        # plt.title("10k Random walk and 1k prior and hill")
+        # plt.xlabel("Cost")
+        # plt.ylabel("Frequency")
+        # plt.legend(loc='upper right')
+        # plt.show()
